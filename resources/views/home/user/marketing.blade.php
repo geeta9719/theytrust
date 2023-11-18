9a7f5936cf6c07cfebaed94ae4d6aa4e2bf2a174
dharmendra kumar <dharmendra@avidusinteractive.com>
2023-11-18 14:28:28 +0530

    fix blade file

--- resources/views/home/user/marketing.blade.php
+++ resources/views/home/user/marketing.blade.php
@@ -1,7 +1,7 @@
 $company = \App\Models\Company::where('user_id', auth()->user()->id)->first();
-    $admin = $company ? \App\Models\AdminInfo::where('company_id', $company->id)->first() : null;
+    $data = $company ? \App\Models\AdminInfo::where('company_id', $company->id)->first() : null;
 @endphp
-@extends($admin ? 'layouts.home-master' : 'layouts.home')
+@extends($data ? 'layouts.home-master' : 'layouts.home')
 
 @section('content')
 
