793353df480db11e8daadc42fa565b6be96c6ddf
dharmendra kumar <dharmendra@avidusinteractive.com>
2024-02-23 13:10:00 +0530

    fix issue

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -9,10 +9,13 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Models\Company;
 use Illuminate\Support\Facades\Redirect;
-
+use App\Events\UserLoggedIn;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 use Socialite;
-use Auth;
+//use Auth;
 use Exception;
 use Session;
 use Cookie;
@@ -47,7 +50,6 @@ class AuthController extends Controller
         return Socialite::driver( 'linkedin2' )->redirect(); 
     }
 
-
     public function handleLinkedinCalimYourProfile()
 {
     try 
@@ -60,21 +62,16 @@ class AuthController extends Controller
         {
             $user_to_be_claimed = User::where('id', $user_id_to_be_claimed)->first();
 
-
-
-            Session::forget( 'claim_profile_id' );
-
-            if( !empty( $user_id_to_be_claimed ) )
+            if (!empty($user_to_be_claimed)) 
             {
-                
                 $existingUser = User::where('email', $user->email)->first();
                 if ($existingUser && $existingUser->id !== $user_id_to_be_claimed) {
                     $company = Company::where('user_id', $user_id_to_be_claimed)->first();
-                     $companySlug = str_replace('+', '-', strtolower(html_entity_decode(urlencode($company['name']))));
-                     return Redirect::to(route('profile', $companySlug))->with('message', 'You have already claimed a listing. One id can claim only one listing. Please connect with support in case of any query/doubt.');
-
+                    $companySlug = str_replace('+', '-', strtolower(html_entity_decode(urlencode($company['name']))));
+                    return Redirect::to(route('profile', $companySlug))->with('message', 'You have already claimed a listing. One id can claim only one listing. Please connect with support in case of any query/doubt.');
                 }
 
+                // Update user details
                 $user_to_be_claimed->name = $user->name;
                 $user_to_be_claimed->email = $user->email;
                 $user_to_be_claimed->linkedin_id = $user->id;
@@ -86,8 +83,12 @@ class AuthController extends Controller
 
                 if ($user_to_be_claimed->save()) 
                 {
-                    $company = Company::where('user_id', $user_id_to_be_claimed)->first();
+                    // Send email notification
+                    Mail::to($user_to_be_claimed->email)->send(new ProfileClaimed());
+
+                    // Login the user
                     Auth::login($user_to_be_claimed);
+
                     return redirect()->back()->with('message', 'Yay.. You have successfully claimed this profile.');
                 } 
                 else 
@@ -99,6 +100,7 @@ class AuthController extends Controller
     } 
     catch (Exception $e) 
     {
+        dd("Sdfsdf");
         dd($e,$e->getMessage());
     }
 }
