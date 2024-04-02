a168c1c89afd32b1a33d9efa38b87b8df0521f72
dharmendra kumar <dharmendra@avidusinteractive.com>
2024-04-02 16:52:29 +0530

    fix issue

--- resources/views/home/user/basicInfo.blade.php
+++ resources/views/home/user/basicInfo.blade.php
@@ -11,7 +11,62 @@ if(isset($_GET['profile']) && !empty($_GET['profile'])){
     $profile_type = '';
 }    
 ?>
+<style>
+    .logocon{
+        font-size:10px;
+        color:red;
+        margin-left: -95px;
+    }
+    .basicinfo h4{
+        text-align:center;
+    }
+    .uploadbox h4{
+        color: #0087f2;;
+        font-size:18px;
+        font-weight:bold;
+    }
+    .uploadbox .upload{
+        display:inline-block; 
+        margin:20px 0;
+    }
+    .companylogo{
+        text-align:right;
+    }
+    .company-form-box{
+       
+    display: flex;
+    /* align-items: center; */
+    justify-items: center;
+    width: 89%;
+    margin: auto;
+    padding-bottom: 30px;
+    }
+    .company-form-box select{
+        background-image: url(../../front_components/images/barrow.jpg);
+    background-repeat: no-repeat;
+    background-position: right;
+    background-size: 26px;
+}
+    
+.btnbasic .btn{
+    background: rgb(1, 104, 236);
+    background: linear-gradient(0deg, rgba(1, 104, 236, 1) 21%, rgba(0, 180, 248, 1) 79%);
+    border: 0;
+    font-weight: 300;
+    font-size: 19px;
+    padding: 8px 85px;
+    border-radius: 3rem;
+    transition: all .3s;
+}
 
+
+
+@media only screen and (max-width: 767px) {
+.company-form-box {
+    display: block;
+}
+}
+</style>
 <section class="container-fluid signin-banner animatedParent hero-section ">
     <div class="container ">
         <div class="row">
@@ -43,20 +98,38 @@ if(isset($_GET['profile']) && !empty($_GET['profile'])){
                     <input type="hidden" id="oldLogo" name="oldLogo" value="{{$company->logo ?? ''}}">
 
                     <div class="card-body sheet" id="sheet1">
-                        <h4><strong class="card-title" >Let's get some basic information</strong></h4> 
-                        <div class="pt-4 file-field">
-                            <img src="<?php if(!empty($company->logo)){ echo $company->logo;}else{echo asset('front_components/images/logo1.jfif');}?>" width="40" height="40" style="border-radius:25px;">
-
-                            <span> Upload Company Logo  </span><strong style="color: red;"> *</strong>
-                            <input type="file" class="rmvId" id="logo" name="logo">
-                            <div class="invalid-feedback error1 logo rmvCls">Invalid Image Format!</div>
+                        <div class="basicinfo">
+                        <h4 class="text-align-center"><strong class="card-title" >Let's get some basic information</strong></h4> 
                         </div>
+                        
+                       <div class="row">
+                       <div class="pt-4 col-md-11 mx-auto d-flex align-item-center">
+                       <div class="pt-4 col-md-8 file-field uploadbox">
+                            
+                            <h4> Upload Company Logo  </h4>
+                          <div class="upload"><strong style="color: red;"> *</strong>  <input type="file" class="rmvId" id="logo" name="logo">
+                            <span class="logocon">(Company Logo should be square jpg, png Format and at least 512 × 512 pixels.) </span></div>
+                            <div class="invalid-feedback error1 logo rmvCls">Invalid Image Format!</div>
+                        </div> 
+                        <div class="pt-4 col-md-4 file-field companylogo" > 
+                            <img src="<?php if(!empty($company->logo)){ echo $company->logo;}else{echo asset('front_components/images/logo1.jfif');}?>" width="200" height="auto" style="border-radius:25px;">
 
+                            </div> 
+                        </div>  
+                         
+                        </div>
+              
+              
+              
+                        <div class="row">
+                        <div class="company-form-box"> 
+                        <div class=" col-md-6 col-12" >
                         <div class="form-group pt-4">
                             <label for="name">Organization Name </label><strong style="color: red;"> *</strong>
                             <input type="text" class="form-control rmvId" id="name" name="name" value="<?php if(!empty($company->name)){ echo $company->name;}?>">
                             <div class="invalid-feedback name rmvCls"></div>
                         </div>
+                      
 
                         <div class="form-group">
                             <label for="website">Website or Company URL (eg: https://example.com) </label><strong style="color: red;"> *</strong>
@@ -94,6 +167,11 @@ if(isset($_GET['profile']) && !empty($_GET['profile'])){
                             <div class="invalid-feedback rate rmvCls"></div>
                         </div>
 
+                       
+                        </div>
+
+
+                        <div class=" col-md-6 col-12 pt-4" >
                         <div class="form-group">
                             <label for="size">Organization Size</label><strong style="color: red;"> *</strong>
                             <select class="form-control rmvId" id="size" name="size" >
@@ -104,7 +182,6 @@ if(isset($_GET['profile']) && !empty($_GET['profile'])){
                             </select>
                             <div class="invalid-feedback size rmvCls"></div>
                         </div>
-
                         <div class="form-group">
                             <label for="founded_at">Company Founded</label>
                             <select class="form-control rmvId" id="founded_at" name="founded_at" >
@@ -118,20 +195,25 @@ if(isset($_GET['profile']) && !empty($_GET['profile'])){
 
                         <div class="form-group">
                             <label for="tagline">Title</label><strong style="color: red;"> *</strong>
-                            <input type="text" class="form-control rmvId" id="tagline" name="tagline" value="<?php if(!empty($company->tagline)){ echo $company->tagline;}?>">
+                            <input type="text" class="form-control rmvId"  placeholder="This will be displayed as main heading of your listing" id="tagline" name="tagline" value="<?php if(!empty($company->tagline)){ echo $company->tagline;}?>">
                             <div class="invalid-feedback tagline rmvCls"></div>
                         </div>
 
                         <div class="form-group">
                             <label for="short_description">Message</label><strong style="color: red;"> *</strong>
-                            <textarea name="short_description" id="short_description" cols="50" rows="5" class="form-control rmvId"><?php if(!empty($company->short_description)){ echo $company->short_description;}?></textarea>
+                            <textarea name="short_description" id="short_description" cols="50" placeholder="Tell us a little bit about your company" rows="5" class="form-control rmvId"><?php if(!empty($company->short_description)){ echo $company->short_description;}?></textarea>
                             <div class="invalid-feedback short_description rmvCls"></div>
                         </div>
+                        
+                       
                     </div>
+                    </div>       </div>   
+                    <div class="col-md-12 text-center btnbasic" > <button type="button" class="btn btn-sm btn-primary" onclick="checkValue()">Next</button></div>    
                 </form>
-                <button type="button" class="btn btn-sm btn-primary" onclick="checkValue()">Next</button>
+               
             </div>
         </div>
+       
     </div>
 </section>                        
 @endsection
@@ -148,7 +230,7 @@ if(isset($_GET['profile']) && !empty($_GET['profile'])){
         checkValue = function(){
             var ser = new FormData($('#basicAdd')[0]);
             //var ser = $('#basicAdd').serialize();
-            //console.log(ser);
+            console.log(ser);
             jQuery.ajax({
                 url:"{{url('get-listed-validation-step')}}",
                 type: "POST",
@@ -162,7 +244,6 @@ if(isset($_GET['profile']) && !empty($_GET['profile'])){
                     $(".rmvCls").html('');
                     $(".rmvId").removeClass('is-invalid');
                     var count = Object.keys(result).length;
-                    //console.log(count);
                     if(count > 0){
                         $.each(result, function (key, value) {
                             //console.log(key);
