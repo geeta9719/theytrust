780c08f3d3ae573ace7244a90fb2c47d30f6f219
dharmendra kumar <dharmendra@avidusinteractive.com>
2024-05-08 11:18:46 +0530

    fix issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue';
-import ExampleComponent from './components/ExampleComponent.vue';
 import FocusComponent from './components/FocusComponent.vue';
 
 const app = createApp({});
 app.component('FocusComponent', FocusComponent);
 app.component('FocusMainComponent', FocusComponent);
-// app.component('ExampleComponent', ExampleComponent);
 app.mount('#app');
 
 
