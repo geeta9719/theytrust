a1fba9f1004b19c68d6ce80697927be7a675d316
dharmendra kumar <dharmendra@avidusinteractive.com>
2024-06-19 15:28:53 +0530

    fix issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import FocusComponent from './components/FocusComponent.vue';
+import IndustryClientForm from './components/Industry.vue';
 
 const app = createApp({});
 app.component('FocusComponent', FocusComponent);
+app.component('industry-client-form', IndustryClientForm);
 app.component('FocusMainComponent', FocusComponent);
 app.mount('#app');
 
