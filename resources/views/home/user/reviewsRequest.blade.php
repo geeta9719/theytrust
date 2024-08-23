f6d09426ef4363dc72a4b5917dc6c58bd266d5e0
geeta9719 <geeta@elixirweb.us>
2024-08-23 09:31:19 +0530

    fix issue

--- resources/views/home/user/reviewsRequest.blade.php
+++ resources/views/home/user/reviewsRequest.blade.php
@@ -1,62 +1,92 @@
 @extends('layouts.home-master')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h3>Request a Review</h3>
+
+<div class="container request-sec">
+    <div class="row ">
+    <h3>Request a Review</h3>
+</div>
+</div>
+
+
+<div class="container-fluid request-sec">
+   <div class="container-fluid m-0 p-0"> 
+    <div class="row ">
+    
+        <div class="col-md-6 mx-auto">
+           
             <div class="card">
                 <div class="card-body">
                     <form action="{{ route('reviews.store') }}" method="POST">
                         @csrf
-                        <div class="form-group">
+                        <div class="form-row row">
+                        <div class="form-group col-md-5">
                             <label for="name">Name</label>
-                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" required>
+                            <input type="text" name="name" placeholder="Enter Name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" required>
                             @error('name')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="form-group">
+                        <div class="form-group col-md-5">
                             <label for="email">Email</label>
-                            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required>
+                            <input type="email" name="email"  placeholder="Enter Email Id" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required>
                             @error('email')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="form-group">
+                        
+                        <div class="form-group col-md-12">
                             <label for="note">Personalized Note</label>
-                            <textarea name="note" class="form-control @error('note') is-invalid @enderror">{{ old('note') }}</textarea>
+                            <textarea name="note"  placeholder="Enter Note" class="form-control @error('note') is-invalid @enderror">{{ old('note') }}</textarea>
                             @error('note')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit Request</button>
+                        <div>
+                        <button type="submit" class="btn btn-primary submit">Submit Request</button>
                     </form>
                 </div>
             </div>
         </div>
-        <div class="col-md-12 mt-5">
-            <h3>Reviews Request History</h3>
+     </div>
+    </div>
+</div>
+
+
+
+
+<div class="container request-sec">
+    <div class="row ">
+    <h3 class="ml-md-3 ml-0">Reviews Request History</h3>
+</div>
+</div>
+
+<div class="container request-sec request-history">
+    <div class="row ">
+    
+       
+        <div class="col-md-12 ">
+            <!-- <h3>Reviews Request History</h3> -->
             <div class="card">
                 <div class="card-body">
                     <table class="table table-striped">
                         <thead>
-                            <tr>
-                                <th>Check</th>
+                            <tr class="heading">
+                                <th class="text-center">Check</th>
                                 <th>S. No.</th>
                                 <th>Date Requested</th>
-                                <th>Client Name</th>
-                                <th>Resend Request</th>
+                                <th >Client Name</th>
+                                <th class="text-center">Resend Request</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach($reviews as $review)
                                 <tr>
-                                    <td><input type="checkbox"></td>
+                                    <td  class="text-center"><input type="checkbox"></td>
                                     <td>{{ $loop->iteration }}</td>
                                     <td>{{ $review->created_at->format('M d, Y') }}</td>
-                                    <td>{{ $review->name }}</td>
-                                    <td>
+                                    <td >{{ $review->name }}</td>
+                                    <td class="text-center">
                                         <form action="{{ route('reviews.resend', $review->id) }}" method="POST">
                                             @csrf
                                             <button type="submit" class="btn btn-link">
@@ -73,4 +103,13 @@
         </div>
     </div>
 </div>
+
+
+
+
+
+
+
+
+
 @endsection
