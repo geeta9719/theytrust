2a78f761e2361157ff0727039279d4ed4914831e
dharmendra kumar <dharmendra@avidusinteractive.com>
2024-10-16 11:00:58 +0530

    Fix issue

--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -6,7 +6,7 @@
 <body>
     <div style="background-color: #f9f9f9; padding: 20px; font-family: Arial, sans-serif;">
         <div style="max-width: 600px; margin: 0 auto; background: white; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
-            <img src="http://127.0.0.1:8000/front_components/images/logo.png" alt="They Trust Us Logo" style="width: 150px; display: block; margin: 0 auto;">
+            <img src="https://theytrust-us.developmentserver.info/front_components/images/logo.png" alt="They Trust Us Logo" style="width: 150px; display: block; margin: 0 auto;">
             <h2 style="text-align: center; color: #333;">Account Verification for They Trust Us</h2>
             <p>Hi {{ $user->first_name }} {{ $user->last_name }},</p>
             <p>Thanks for signing up for "They Trust Us". Before we get started, please click the button below to verify your email address. This link will expire after 7 days.</p>
