ed8633aa5d11fcaf273bc485dd29c69a97eeda24
dharmendra kumar <dharmendra@avidusinteractive.com>
2024-10-29 18:29:38 +0530

    Fix issue

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1099,8 +1099,7 @@ public function dashboard(Request $request, $company)
 
     public function getCity(Request $request)
     {
-        // dd($request->state_code);
-        $data['cities'] = City::where("state_code", $request->state_code)->get(["name", "id"]);
+        $data['cities'] = City::where("state_code", $request->state_code)->orderBy("name", 'asc'->get(["name", "id"]);
         return response()->json($data);
     }
 
