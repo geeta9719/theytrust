ec84a991ab2150562108dbfaff55e18ccaf7b93c
dharmendra kumar <dharmendra@avidusinteractive.com>
2024-12-06 09:24:44 +0530

    fix issue

--- app/Http/Controllers/PaymentContorller.php
+++ app/Http/Controllers/PaymentContorller.php
@@ -162,6 +162,7 @@ class PaymentContorller extends Controller
             $subscription = $this->subscribeToPlan($plan, $user, false);
             $this->sendEmailWithPdf($user->id);
             $company = Company::with('user')->where('id', $userId)->first();
+            Log::info('Membership points updated: ', $company);
             if ($company->id) {
                 $membershipPointsResult = CompanyPointHelper::processMembershipPoints($company->id, $plan->name);
                 Log::info('Membership points updated: ', $membershipPointsResult);
