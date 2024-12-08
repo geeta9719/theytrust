5dd3d2569abe76d41e6a9db539d4eb8a4a94a2f2
dharmendra kumar <dharmendra@avidusinteractive.com>
2024-12-08 20:45:31 +0530

    fix issue

--- app/Http/Controllers/PaymentContorller.php
+++ app/Http/Controllers/PaymentContorller.php
@@ -149,20 +149,20 @@ class PaymentContorller extends Controller
             Log::info('ddddddddddddddddddddddddddddddddd points updated: ');
 
             // Use a database transaction to ensure data integrity
-            // DB::transaction(function () use ($paymentStatus, $amount, $currency, $planId, $userId) {
-            //     // Create a new transaction record
-            //     $transaction = new Transaction();
-            //     $transaction->payment_status = $paymentStatus;
-            //     $transaction->amount = $amount;
-            //     $transaction->currency = $currency;
-            //     $transaction->plan_id = $planId;
-            //     $transaction->user_id = $userId;
-            //     $transaction->save();
-            // });
+            DB::transaction(function () use ($paymentStatus, $amount, $currency, $planId, $userId) {
+                // Create a new transaction record
+                $transaction = new Transaction();
+                $transaction->payment_status = $paymentStatus;
+                $transaction->amount = $amount;
+                $transaction->currency = $currency;
+                $transaction->plan_id = $planId;
+                $transaction->user_id = $userId;
+                $transaction->save();
+            });
             $plan = PlanModel::find($planId);
             $user = User::find($userId);
             $subscription = $this->subscribeToPlan($plan, $user, false);
-            // $this->sendEmailWithPdf($user->id);
+            $this->sendEmailWithPdf($user->id);
             // Log::info('userIduser: ',$userId,";;;;;");
             $company = Company::where('user_id', $userId)->first();
             // Log::info('userIduserId: ',$company->id,$company[0],";;;;;");
