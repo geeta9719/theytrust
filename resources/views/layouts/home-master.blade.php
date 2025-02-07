73e60c43ee04e75cf885bf72d064f232761cae78
dharmendra kumar <dharmendra@avidusinteractive.com>
2025-02-07 15:11:01 +0530

    fix issue

--- resources/views/layouts/home-master.blade.php
+++ resources/views/layouts/home-master.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Page Title</title>
     
@@ -39,6 +40,13 @@
     @include('home.partials._foot')
     @include('home.partials._footer')
     
+    <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "q61lnt6ajy");
+    </script>
     @yield('script')
 </body>
 </html>
