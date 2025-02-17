ac5f73dddef23068f664bf8d8981662c7a6fcf68
dharmendra kumar <dharmendra@avidusinteractive.com>
2025-02-17 11:00:18 +0530

    fix ssue

--- resources/views/home/partials/_header.blade.php
+++ resources/views/home/partials/_header.blade.php
@@ -25,5 +25,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Custom Style -->
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}" />
+    {{-- <link rel="stylesheet" href="{{ asset('/css/style.css') }}" /> --}}
 </head>
