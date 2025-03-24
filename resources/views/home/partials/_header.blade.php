014dfdd98376b4c18ff39c3102ff81102032dea4
Your Name <you@example.com>
2025-03-24 12:21:24 +0530

    fix issue

--- resources/views/home/partials/_header.blade.php
+++ resources/views/home/partials/_header.blade.php
@@ -13,6 +13,7 @@
         rel="stylesheet"
     />
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
 
     <!-- External Stylesheets -->
