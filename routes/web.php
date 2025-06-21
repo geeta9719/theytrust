3c68b8b0c36e59022616f2c862135bc30c0b51da
Your Name <you@example.com>
2025-06-21 16:14:10 +0530

    fix issue

--- routes/web.php
+++ routes/web.php
@@ -384,7 +384,7 @@ Route::middleware([ 'check.company'])->group(function () {
 Route::get('/terms-of-use', [PageController::class, 'termsOfUse'])->name('terms-of-use');
 Route::get('/privacy-policy', [PageController::class, 'privacyPolicy'])->name('privacy-policy');
 Route::get('/blogs', [BlogController::class, 'index'])->name('blogs.list');
-Route::get('/blog/{id}', [BlogController::class, 'show'])->name('blogs.show');
+Route::get('/blog-summary/{id}', [BlogController::class, 'show'])->name('blogs.show');
 
 
 Route::get('/generate-company-review', [HomeController::class, 'generateSingleCompanyReview'])->name('generateSingleCompanyReview');
